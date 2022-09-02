611cde3f20e4bec0d030d4c03c15eeb05f8cfc0e
Evaldas <evaldas.visockas@visma.com>
2022-09-02 21:12:23 +0300

    Add case controller [get all cases, get case data by its id, create a case], add document controller [get documents' data by case id, get attachments' data by document id]

---
+++ WebApp/src/main/java/ev/projects/webapp/rest_controllers/CaseController.java
@@ -0,0 +1,37 @@
+package ev.projects.webapp.rest_controllers;
+
+import ev.projects.Case;
+import ev.projects.webapp.repositories.CaseRepository;
+import ev.projects.webapp.repositories.EntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CaseController {
+
+    private EntityRepository<Case> caseRepository;
+
+    @Autowired
+    public CaseController(CaseRepository caseRepository) {
+        this.caseRepository = caseRepository;
+    }
+
+    @GetMapping("/cases")
+    public List<Case> getCases() {
+        return caseRepository.getAll();
+    }
+
+    @GetMapping("/case")
+    public Optional<Case> getCase(@RequestParam("id") long ID) {
+        return caseRepository.getByID(ID);
+    }
+
+    @PostMapping("/cases/create")
+    public void saveCase(@RequestBody Case caseToSave) {
+        caseRepository.save(caseToSave);
+    }
+
+
+}
---
+++ WebApp/src/main/java/ev/projects/webapp/rest_controllers/DocumentController.java
@@ -0,0 +1,45 @@
+package ev.projects.webapp.rest_controllers;
+
+import ev.projects.Attachment;
+import ev.projects.Case;
+import ev.projects.Document;
+import ev.projects.webapp.repositories.EntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class DocumentController {
+
+    private EntityRepository<Document> documentRepository;
+    private EntityRepository<Case> caseRepository;
+
+    @Autowired
+    public DocumentController(EntityRepository<Document> documentRepository, EntityRepository<Case> caseRepository) {
+        this.documentRepository = documentRepository;
+        this.caseRepository = caseRepository;
+    }
+
+    @GetMapping("/case/documents")
+    public List<Document> getDocuments(@RequestParam("id") long caseID) {
+        Optional<Case> foundCase = caseRepository.getByID(caseID);
+        if(foundCase.isPresent()) {
+            return foundCase.get().getDocuments();
+        }
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/case/documents/attachments")
+    public List<Attachment> getAttachments(@RequestParam("id") long documentID) {
+        Optional<Document> foundDocument = documentRepository.getByID(documentID);
+        if(foundDocument.isPresent()) {
+            return foundDocument.get().getAttachments();
+        }
+        return new ArrayList<>();
+    }
+
+}
