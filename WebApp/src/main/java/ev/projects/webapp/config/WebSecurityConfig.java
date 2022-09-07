fbc81a338847eee9b3f3636778b9a9f4fd5cc881
Evaldas <evaldas.visockas@visma.com>
2022-09-07 14:10:56 +0300

    Change hardcoded authorization to db authorization

--- WebApp/src/main/java/ev/projects/webapp/config/WebSecurityConfig.java
+++ WebApp/src/main/java/ev/projects/webapp/config/WebSecurityConfig.java
@@ -1,30 +1,37 @@
 package ev.projects.webapp.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import ev.projects.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableJpaRepositories(basePackageClasses = IUserRepository.class)
 public class WebSecurityConfig {
 
-    @Value("${login.username}")
-    private String username;
-    @Value("${login.hashed_password}")
-    private String hashedPassword;
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public WebSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+        httpSecurity.csrf().disable();
+        httpSecurity.authorizeRequests().antMatchers("/api/users/register/")
+                .anonymous().anyRequest().authenticated()
+                .and().formLogin()
+                .and().httpBasic();
         return httpSecurity.build();
     }
 
@@ -34,9 +41,11 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder().username(username).password(hashedPassword).roles("USER").build();
-        return new InMemoryUserDetailsManager(user);
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(getPasswordEncoder());
+        return authProvider;
     }
 
 }
