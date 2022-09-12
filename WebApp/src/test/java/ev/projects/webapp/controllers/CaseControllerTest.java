ccdfb745cfc8fc0767f6e3bd8de3a25d710b951c
Evaldas <evaldas.visockas@visma.com>
2022-09-12 07:26:29 +0300

    Make db offset in case controller to allow all test be launched at once

--- WebApp/src/test/java/ev/projects/webapp/controllers/CaseControllerTest.java
+++ WebApp/src/test/java/ev/projects/webapp/controllers/CaseControllerTest.java
@@ -3,16 +3,11 @@ package ev.projects.webapp.controllers;
 import ev.projects.models.Case;
 import ev.projects.models.User;
 import ev.projects.repositories.ICaseRepository;
-import ev.projects.services.ICaseService;
-import ev.projects.services.IUserService;
+import ev.projects.repositories.IUserRepository;
+import ev.projects.services.UserService;
 import ev.projects.webapp.WebAppApplication;
-import org.apache.coyote.Response;
-import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -21,11 +16,10 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import static ev.projects.webapp.utils.EntitiesCreator.createCase;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -38,7 +32,8 @@ public class CaseControllerTest {
 
     private String baseUrl;
 
-    private IUserService userService;
+    private IUserRepository userRepository;
+    private UserService userService;
     private ICaseRepository caseRepository;
 
     String username1 = "Smith";
@@ -46,22 +41,28 @@ public class CaseControllerTest {
     String password1 = "pass";
     String password2 = "smith";
 
+    private long userCountOffset;
+    private long caseCountOffset;
+
     @BeforeAll
     public void startUp() {
         loadData();
     }
 
     @Autowired
-    public CaseControllerTest(IUserService userService, ICaseRepository caseRepository) {
+    public CaseControllerTest(IUserRepository userRepository, UserService userService, ICaseRepository caseRepository) {
+        this.userRepository = userRepository;
         this.userService = userService;
         this.caseRepository = caseRepository;
     }
 
     public void loadData() {
-        userService.add(new User(username1, password1));
+        userCountOffset = userService.add(new User(username1, password1)).getID() - 1;
         userService.add(new User(username2, password2));
-        caseRepository.save(createCase("English", "English lessons", userService.getByUsername(username1).get()));
-        caseRepository.save(createCase("Math", "Math horrors", userService.getByUsername(username2).get()));
+        caseCountOffset = caseRepository.save(createCase("English", "English lessons",
+                userService.getByUsername(username1).get())).getID() - 1;
+        caseRepository.save(createCase("Math", "Math horrors",
+                userService.getByUsername(username2).get()));
     }
 
     @BeforeEach
@@ -69,24 +70,15 @@ public class CaseControllerTest {
         baseUrl = "http://localhost:" + port + "/api/cases/";
     }
 
-    private Case createCase(String title, String description, User user) {
-        Case aCase = new Case();
-        aCase.setTitle(title);
-        aCase.setDescription(description);
-        aCase.setCreatorUser(user);
-        aCase.setCreationDate(new Date());
-        return aCase;
-    }
-
     @Test
     public void getCasesTest() {
         TestRestTemplate restTemplate = new TestRestTemplate().withBasicAuth(username1, password1);
-        assertCase(restTemplate, "Math", "Math horrors", username2,1);
+        assertCase(restTemplate, "Math", "Math horrors", username2, (int) (caseCountOffset + 1));
     }
 
     @Test
     public void getCaseTest() {
-        String url = baseUrl + "/1/";
+        String url = baseUrl + "/" + (caseCountOffset + 1) + "/";
         TestRestTemplate restTemplate = new TestRestTemplate().withBasicAuth(username1, password1);
         ResponseEntity<Optional<Case>> response = restTemplate.exchange(url, HttpMethod.GET,
                 null, new ParameterizedTypeReference<>() {});
@@ -97,9 +89,9 @@ public class CaseControllerTest {
         Case aCase = responseCase.get();
         assertEquals( "English", aCase.getTitle());
         assertEquals( "English lessons", aCase.getDescription());
-        assertEquals( 1, aCase.getID());
+        assertEquals( caseCountOffset + 1, aCase.getID());
         assertNotNull(aCase.getCreatorUser());
-        assertEquals( 1, aCase.getCreatorUser().getID());
+        assertEquals( userCountOffset + 1, aCase.getCreatorUser().getID());
         assertEquals(username1, aCase.getCreatorUser().getName());
     }
 
@@ -113,13 +105,13 @@ public class CaseControllerTest {
         ResponseEntity<Case> createResponse = restTemplate.exchange(baseUrl, HttpMethod.POST,
                 createRequest, new ParameterizedTypeReference<>() {});
         assertEquals(HttpStatus.OK, createResponse.getStatusCode());
-        assertCase(restTemplate, title, description, username1,2);
+        assertCase(restTemplate, title, description, username1, (int) (caseCountOffset + 2));
     }
 
     @Test
     @Order(2)
     public void updateCaseTest() {
-        String url = baseUrl + "/3/";
+        String url = baseUrl + "/" + (caseCountOffset + 3) + "/";
         String title = "Future";
         String description = "Legendary tales";
         TestRestTemplate restTemplate = new TestRestTemplate().withBasicAuth(username1, password1);
@@ -127,7 +119,7 @@ public class CaseControllerTest {
         ResponseEntity<Case> createResponse = restTemplate.exchange(url, HttpMethod.PUT,
                 updateRequest, new ParameterizedTypeReference<>() {});
         assertEquals(createResponse.getStatusCode(), HttpStatus.OK);
-        assertCase(restTemplate, title, description, username1, 2);
+        assertCase(restTemplate, title, description, username1, (int) (caseCountOffset + 2));
     }
 
     private void assertCase(TestRestTemplate restTemplate, String title, String description, String username, int index) {
@@ -136,24 +128,24 @@ public class CaseControllerTest {
         assertEquals(getResponse.getStatusCode(), HttpStatus.OK);
         assertNotNull(cases);
         assertEquals(index + 1, cases.size());
-        assertNotNull(cases.get(index));
-        assertNotNull(cases.get(index).getCreatorUser());
-        assertEquals(username, cases.get(index).getCreatorUser().getName());
-        assertEquals(index + 1, cases.get(index).getID());
-        assertEquals(title, cases.get(index).getTitle());
-        assertEquals(description, cases.get(index).getDescription());
+        Case aCase = cases.get(index);
+        assertNotNull(aCase);
+        assertNotNull(aCase.getCreatorUser());
+        assertEquals(index + 1, aCase.getID());
+        assertEquals(username, aCase.getCreatorUser().getName());
+        assertEquals(title, aCase.getTitle());
+        assertEquals(description, aCase.getDescription());
     }
 
     @Test
     public void deleteCaseTest() {
         TestRestTemplate restTemplate = new TestRestTemplate().withBasicAuth(username1, password1);
-        String deleteUrl = baseUrl + "/3/";
-        ResponseEntity<Void> deleteResponse = restTemplate.exchange(deleteUrl, HttpMethod.DELETE,
+        String url = baseUrl + "/" + (caseCountOffset + 3) + "/";
+        ResponseEntity<Void> deleteResponse = restTemplate.exchange(url, HttpMethod.DELETE,
                 null, new ParameterizedTypeReference<>() {});
         assertEquals(deleteResponse.getStatusCode(), HttpStatus.OK);
-        String getAfterDeleteUrl = baseUrl + "/3/";
         ResponseEntity<Optional<Case>> getResponse =
-                restTemplate.exchange(getAfterDeleteUrl, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+                restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
         Optional<Case> deletedCase = getResponse.getBody();
         assertNotNull(deletedCase);
         assertFalse(deletedCase.isPresent());
