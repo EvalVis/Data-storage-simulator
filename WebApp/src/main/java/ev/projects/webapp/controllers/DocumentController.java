3ac7747074075ba75754f4e4c3c33fb9382e7c5d
Evaldas <evaldas.visockas@visma.com>
2022-09-13 15:40:55 +0300

    Add document page with upload of file

--- WebApp/src/main/java/ev/projects/webapp/controllers/DocumentController.java
+++ WebApp/src/main/java/ev/projects/webapp/controllers/DocumentController.java
@@ -1,15 +1,14 @@
 package ev.projects.webapp.controllers;
 
-import ev.projects.models.Case;
 import ev.projects.models.Document;
 import ev.projects.services.IDocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import static ev.projects.models.DocumentFactory.createDocumentOfCase;
 
 @Controller
 @RequestMapping("/documents")
@@ -33,13 +32,25 @@ public class DocumentController {
     @GetMapping("/add-document/{case_id}")
     public String getAddDocumentForm(Model model, @PathVariable("case_id") Long caseID) {
         model.addAttribute("document", new Document());
+        model.addAttribute("case_id", caseID);
         return "add_document";
     }
 
     @PostMapping("/add-document")
-    public String createCase(@ModelAttribute Document document) {
+    public String createDocument(@ModelAttribute Document document) {
         long ID = documentService.add(document).getID();
         return "redirect:/documents/" + ID;
+    }
+
+    @PostMapping("/{id}")
+    public String uploadDocument(@PathVariable("id") long ID, @RequestParam("file") MultipartFile file) {
+        try {
+            documentService.uploadDocument(ID, file);
+            return "redirect:/documents/" + ID;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
     }
 
 }
