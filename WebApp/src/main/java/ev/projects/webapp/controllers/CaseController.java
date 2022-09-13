db311eeefd21c11099712ee43f86738baea5774c
Evaldas <evaldas.visockas@visma.com>
2022-09-13 11:40:38 +0300

    Add case page with info table

--- WebApp/src/main/java/ev/projects/webapp/controllers/CaseController.java
+++ WebApp/src/main/java/ev/projects/webapp/controllers/CaseController.java
@@ -4,9 +4,8 @@ import ev.projects.services.ICaseService;
 import ev.projects.webapp.forms.SearchFormData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/cases")
@@ -22,6 +21,14 @@ public class CaseController {
     @PostMapping("/get-case")
     public String redirectToCasePage(@ModelAttribute SearchFormData searchFormData) {
         return "redirect:/cases/" + searchFormData.getID();
+    }
+
+    @GetMapping("/{id}")
+    public String showCase(@PathVariable("id") long ID, Model model) {
+        return caseService.getById(ID).map(c -> {
+            model.addAttribute("case", c);
+            return "case";
+        }).orElse("redirect:/");
     }
 
 }
