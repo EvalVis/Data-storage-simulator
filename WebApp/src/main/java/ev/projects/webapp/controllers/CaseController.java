6fb7463b59c826a9bb167f0d561a68ae1acd1751
Evaldas <evaldas.visockas@visma.com>
2022-09-13 11:21:36 +0300

    Add dropdown list to select a case and get the selection into object

--- WebApp/src/main/java/ev/projects/webapp/controllers/CaseController.java
+++ WebApp/src/main/java/ev/projects/webapp/controllers/CaseController.java
@@ -4,26 +4,24 @@ import ev.projects.services.ICaseService;
 import ev.projects.webapp.forms.SearchFormData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/cases")
+public class CaseController {
 
     private ICaseService caseService;
 
     @Autowired
-    public HomeController(ICaseService caseService) {
+    public CaseController(ICaseService caseService) {
         this.caseService = caseService;
     }
 
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("cases", caseService.getAll());
-        model.addAttribute("searchFormData", new SearchFormData());
-        return "home";
+    @PostMapping("/get-case")
+    public String redirectToCasePage(@ModelAttribute SearchFormData searchFormData) {
+        return "redirect:/cases/" + searchFormData.getID();
     }
 
 }
---
+++ WebApp/src/main/java/ev/projects/webapp/forms/SearchFormData.java
@@ -0,0 +1,12 @@
+package ev.projects.webapp.forms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchFormData {
+
+    private long ID;
+
+}
