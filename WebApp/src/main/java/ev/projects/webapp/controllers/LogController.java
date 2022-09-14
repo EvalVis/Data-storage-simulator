aa6ade42c3a084bb61ecc0de468143a80e21adf4
Evaldas <evaldas.visockas@visma.com>
2022-09-14 10:08:42 +0300

    Add feature to download a report

--- WebApp/src/main/java/ev/projects/webapp/controllers/LogController.java
+++ WebApp/src/main/java/ev/projects/webapp/controllers/LogController.java
@@ -5,8 +5,10 @@ import ev.projects.webapp.forms.SearchFormData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/logs")
@@ -24,6 +26,22 @@ public class LogController {
         model.addAttribute("logs", logService.getAll());
         model.addAttribute("searchFormData", new SearchFormData());
         return "logs";
+    }
+
+    @PostMapping("/download")
+    public String redirectToCasePage(@ModelAttribute SearchFormData searchFormData) {
+        return "redirect:/logs/download/" + searchFormData.getID();
+    }
+
+    @GetMapping("/download/{id}")
+    public void downloadReport(@PathVariable("id") Long ID, HttpServletResponse response) {
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; filename=report.csv");
+        try {
+            logService.getLog(ID, response.getWriter());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
