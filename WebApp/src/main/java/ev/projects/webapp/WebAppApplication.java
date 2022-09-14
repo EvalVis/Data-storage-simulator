f2c783477e01390e1bb3687919520d4eacde0bb4
Evaldas <evaldas.visockas@visma.com>
2022-09-14 15:57:12 +0300

    Prep war for deployment to Tomcat 9

--- WebApp/src/main/java/ev/projects/webapp/WebAppApplication.java
+++ WebApp/src/main/java/ev/projects/webapp/WebAppApplication.java
@@ -2,11 +2,19 @@ package ev.projects.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class WebAppApplication {
+public class WebAppApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(WebAppApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(WebAppApplication.class, args);
     }
